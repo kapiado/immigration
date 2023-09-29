48afb358d813f11a7acc73b901470e133dd306fc
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-09-29 11:00:51 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -29,12 +29,12 @@ button1 = sideb.button("Home")
 button2 = sideb.button("Overview")
 button3 = sideb.button("Profiles")
 
-if button1:
-    python "Home.py"
-elif button2:
-    python "1_Overview.py"
-elif button3:
-    python "2_Average_Waiting_Time.py"
+#if button1:
+    #python "Home.py"
+#elif button2:
+    #python "1_Overview.py"
+#elif button3:
+    #python "2_Average_Waiting_Time.py"
 
 st.write("# Analysis of Factors Affecting U.S. Permanent Residency Using Data and Predictive Analytics")
 image = Image.open('green card.jfif')

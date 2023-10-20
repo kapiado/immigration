575a1d1755be5a9ba102756260640722828e9011
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-10-20 10:38:20 -0700

    Update 1_Overview.py

--- 1_Overview.py
+++ 1_Overview.py
@@ -7,28 +7,29 @@ Created on Wed May 31 18:26:27 2023
 
 import streamlit as st
 import streamlit.components.v1 as components
+from PIL import Image
 
 
 def overview():
     st.set_page_config(page_title="Overview")
     st.write("# Overview")
 
-path = "C:/Users/kapia/multipage_app/HTML Files/"
+    #path = "C:/Users/kapia/multipage_app/HTML Files/"
 
-def cases():
-    HtmlFile = open(path+"CasesReceived.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    print(source_code)
-    components.html(source_code,height=480)
+    def cases():
+        HtmlFile = open("CasesReceived.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        print(source_code)
+        components.html(source_code,height=480)
+        
+    def avgwait():    
+        HtmlFile = open("AvgWaitingTime.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        print(source_code)
+        components.html(source_code,height=600)
     
-def avgwait():    
-    HtmlFile = open(path+"AvgWaitingTime.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    print(source_code)
-    components.html(source_code,height=600)
-
-cases()
-avgwait()
-
-a = st.empty()
-a.write("The North American Industry Classification System (NAICS) is a standardized system used to classify business establishments based on their economic activity in Canada, Mexico, and the United States. It provides a hierarchical structure that groups businesses into various sectors, subsectors, industry groups, and industries. The NAICS code is a unique numerical identifier assigned to each business entity, allowing for consistent and comparable data collection and analysis across different industries and regions.")
+    cases()
+    avgwait()
+    
+    a = st.empty()
+    a.write("The North American Industry Classification System (NAICS) is a standardized system used to classify business establishments based on their economic activity in Canada, Mexico, and the United States. It provides a hierarchical structure that groups businesses into various sectors, subsectors, industry groups, and industries. The NAICS code is a unique numerical identifier assigned to each business entity, allowing for consistent and comparable data collection and analysis across different industries and regions.")

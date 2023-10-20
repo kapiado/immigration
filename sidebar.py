3bdfc359bb45016e63b2951215a671a2584b45ca
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-10-20 10:56:02 -0700

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -15,21 +15,22 @@ import streamlit as st
 
 
 def sideBar():
-    tabs = ["Home", "Predictive Dashboard", "Methodology", "Data Description", "Data Cleaning", "Clustering", "Machine Learning", "Results"]
+    tabs = ["Home", "Overview"]
 
     st.sidebar.subheader("Navigation")
     home_button = st.sidebar.button("Home", key="Home", type="secondary")
-    dashboard_button = st.sidebar.button("Predictive Dashboard", key="Predictive Dashboard", type="secondary")
+    overview_button = st.sidebar.button("Overview", key="Overview", type="secondary")
+    #dashboard_button = st.sidebar.button("Predictive Dashboard", key="Predictive Dashboard", type="secondary")
     st.write(" ")
-    st.sidebar.subheader("Methodology")
-    data_description_button = st.sidebar.button("Data Description", key="Data Description", type="secondary")
-    data_cleaning_button = st.sidebar.button("Data Cleaning", key="Data Cleaning", type="secondary")
-    clustering_button = st.sidebar.button("Clustering", key="Clustering", type="secondary")
-    ml_button = st.sidebar.button("Machine Learning", key="Machine Learning", type="secondary")
-    results_button = st.sidebar.button("Results", key="Results", type="secondary")
+    #st.sidebar.subheader("Methodology")
+    #data_description_button = st.sidebar.button("Data Description", key="Data Description", type="secondary")
+    #data_cleaning_button = st.sidebar.button("Data Cleaning", key="Data Cleaning", type="secondary")
+    #clustering_button = st.sidebar.button("Clustering", key="Clustering", type="secondary")
+    #ml_button = st.sidebar.button("Machine Learning", key="Machine Learning", type="secondary")
+    #results_button = st.sidebar.button("Results", key="Results", type="secondary")
 
     # Store the buttons in a list
-    selection = [home_button, dashboard_button, data_description_button, data_cleaning_button, clustering_button, ml_button, results_button]
+    selection = [home_button, overview_button]
 
     # Return the list of buttons
     return selection

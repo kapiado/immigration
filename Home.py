e4eabf50253c82290dd33aefe9330594cbffe19e
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-10-20 10:50:57 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,101 +14,56 @@ import pandas as pd
 from PIL import Image
 from streamlit_extras import switch_page
 #from streamlit_extras.switch_page_button import switch_page
-st.set_page_config(
-    page_title="Home",
-    page_icon="👋",
-)
+#st.set_page_config(
+#    page_title="Home",
+#    page_icon="👋",
+#)
 #st.sidebar.success("Select a page above.")
 #st.sidebar.title("Home")
-switch_page("Overview")
+#switch_page("Overview")
 
-sideb = st.sidebar
-button1 = sideb.button("Home")
-button2 = sideb.button("Overview")
-button3 = sideb.button("Profiles")
+def home():
 
-if button2:
-    switch_page("Overview")
-
-#if button1:
-    #python "Home.py"
-#elif button2:
-    #python "1_Overview.py"
-#elif button3:
-    #python "2_Average_Waiting_Time.py"
-
-st.write("# Analysis of Factors Affecting U.S. Permanent Residency Using Data and Predictive Analytics")
-image = Image.open('green card.jfif')
-
-st.image(image,caption='Figure 1: Green Card Resident Image (Amazon)')
-
-
-
-
-# df = pd.read_csv("PERM_Data_5_22_23.csv")
-
-# Cases received vs. Years
-# def cases(dataframe):
-#     #uniqueyears = df["CASE_RECEIVED_YEAR"].unique() # Obtain years for x-axis
-#     #years = list(uniqueyears) # Put into list
-#     numcases= df["CASE_RECEIVED_YEAR"].value_counts().sort_index() # Number of cases per year
-#     fig = px.bar(x = numcases.index, y = numcases, title='Cases Received from 2006-2021',height=500,color=numcases)
-#     fig.update_xaxes(title='Year',type='category')
-#     fig.update_yaxes(title='Total Received',type='linear')
-#     st.plotly_chart(fig)
-
-def cases():
-    HtmlFile = open("CasesReceived.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    print(source_code)
-    components.html(source_code,height=600)
+    st.write("# Analysis of Factors Affecting U.S. Permanent Residency Using Data and Predictive Analytics")
+    image = Image.open('green card.jfif')
     
-def avgwait():
-    # Convert to datetime format for subtraction
-    # df["DECISION_DATE"] = pd.to_datetime(df["DECISION_DATE"])
-    # df["CASE_RECEIVED_DATE"] = pd.to_datetime(df["CASE_RECEIVED_DATE"])
-    # # Create new column for waiting time
-    # df["WAITING_TIME"] = df["DECISION_DATE"]-df["CASE_RECEIVED_DATE"]
-    # # Convert column to string and remove days and timestamp
-    # waitstr = df["WAITING_TIME"].astype(str).str.replace('days.+','')
-    # # Create a new column to get only days
-    # df["WAITING_TIME_DAYS"] = waitstr
-    # # Make column int
-    # df["WAITING_TIME_DAYS"] = df["WAITING_TIME_DAYS"].astype(int)
-    # average_waiting_time = df.groupby('CASE_RECEIVED_YEAR')['WAITING_TIME_DAYS'].mean()
-    # numcases= df["CASE_RECEIVED_YEAR"].value_counts().sort_index() # Number of cases per year
-    # fig = px.bar(x = numcases.index, y = average_waiting_time, title='Average Waiting Time from 2006-2021',height=500,color=average_waiting_time)
-    # fig.update_xaxes(title='Year',type='category')
-    # fig.update_yaxes(title='Average Waiting Time (days)',type='linear')
-    # st.plotly_chart(fig)
-    HtmlFile = open("AvgWaitingTime.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    print(source_code)
-    components.html(source_code,height=600)
+    st.image(image,caption='Figure 1: Green Card Resident Image (Amazon)')
     
-st.header('Background')
-p = open("CasesReceived.html")
-components.html(p.read(),height=600)
-c = st.empty()
-c.write('The immigration backlog is a result of the accumulation of immigration applications that have not been processed within a reasonable timeframe. \nIt is caused by increased demand, insufficient resources, complex procedures, and policy changes. Backlogs lead to delays in family reunification, economic impact, strain on resources, and uncertainty for individuals. In this dashboard, we aim to achieve transparency for individuals that are in the middle of the process and want to begin the process for residency. We will be  looking at historical data and factors that influence the wait times in this process, and provide an interface where users can predict their personal waiting time based on their demographics.')
-
-#new line and research objectives content
-st.write("")
-st.header('Research Objectives')
-st.subheader('Descriptive')
-st.subheader('Prescriptive')
-
-#new line and team intro
-st.write("")
-st.header('The Team')
-st.write('**Team Members:** Katrina Apiado, Mahek Karachandani, Nika Mahdavi, & Boaz Nakhimovsky')
-st.write('**Advisor:** Dr. German Serna & Dr. Liz Thompson')
-st.write('**Sponsor:** Dr. Puneet Agarwal')
-
-
-st.write("")
-d = st.empty()
-st.header('Reference List')
-st.write("")
-e = st.empty()
-e.write('Galetti, Beth. “Amazon Urges Green Card Allocation to Help Immigrant Employees.” Amazon, Aug. 2022, www.aboutamazon.com/news/policy-news-views/amazon-urges-green-card-allocation-to-help-immigrant-employees.')
+    def cases():
+        HtmlFile = open("CasesReceived.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        print(source_code)
+        components.html(source_code,height=600)
+        
+    def avgwait():
+        HtmlFile = open("AvgWaitingTime.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        print(source_code)
+        components.html(source_code,height=600)
+        
+    st.header('Background')
+    p = open("CasesReceived.html")
+    components.html(p.read(),height=600)
+    c = st.empty()
+    c.write('The immigration backlog is a result of the accumulation of immigration applications that have not been processed within a reasonable timeframe. \nIt is caused by increased demand, insufficient resources, complex procedures, and policy changes. Backlogs lead to delays in family reunification, economic impact, strain on resources, and uncertainty for individuals. In this dashboard, we aim to achieve transparency for individuals that are in the middle of the process and want to begin the process for residency. We will be  looking at historical data and factors that influence the wait times in this process, and provide an interface where users can predict their personal waiting time based on their demographics.')
+    
+    #new line and research objectives content
+    st.write("")
+    st.header('Research Objectives')
+    st.subheader('Descriptive')
+    st.subheader('Prescriptive')
+    
+    #new line and team intro
+    st.write("")
+    st.header('The Team')
+    st.write('**Team Members:** Katrina Apiado, Mahek Karachandani, Nika Mahdavi, & Boaz Nakhimovsky')
+    st.write('**Advisor:** Dr. German Serna & Dr. Liz Thompson')
+    st.write('**Sponsor:** Dr. Puneet Agarwal')
+    
+    
+    st.write("")
+    d = st.empty()
+    st.header('Reference List')
+    st.write("")
+    e = st.empty()
+    e.write('Galetti, Beth. “Amazon Urges Green Card Allocation to Help Immigrant Employees.” Amazon, Aug. 2022, www.aboutamazon.com/news/policy-news-views/amazon-urges-green-card-allocation-to-help-immigrant-employees.')

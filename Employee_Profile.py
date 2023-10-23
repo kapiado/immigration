b311af12a575fdc4a3aec8fcb5090ac1e156db0c
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-10-23 09:53:29 -0700

    Update Employee_Profile.py

--- Employee_Profile.py
+++ Employee_Profile.py
@@ -10,8 +10,8 @@ import streamlit.components.v1 as components
 from PIL import Image
 
 def employeeprofile():
-        
-        st.markdown("# Employee Profile")
+
+	st.markdown("# Employee Profile")
             
         c = st.empty()
         c.write("The Employee Profile includes key information about the demographic of employees in our dataset, who undergo the green card application process in the United States. This includes their education, salary, nationality, profession, major and layoff history. In the profile, we present visual representations of the data related to Immigration Backlog. We will explore the trend of immigration backlog overtime, in terms of applications received and waiting time for approval, for example. By examining these factors, we aim to gain insights into the patterns and changes within the immigration backlog, enabling a better understanding.")
@@ -105,7 +105,7 @@ def employeeprofile():
                 WTvsSalary()
                 
         with tab5:
-                        unitofpay()
+                unitofpay()
                 
         with tab6:
                 HighestEducation()

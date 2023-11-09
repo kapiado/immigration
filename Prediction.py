e7ed57acea5fce3bf8b2e7ee295ba4acffd590a1
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-08 18:14:48 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -36,9 +36,9 @@ def predictionmodel():
          
          # Cache the dataframe so it's only loaded once
          @st.cache_data
-         def load_data(filename):
-           df = pd.read_csv(filename)
-           return df
+         def load_data(filename)
+                  df = pd.read_csv(filename)
+                    return df
 
          data_format = load_data('penguins_sample.csv')
          st.dataframe(data_format, hide_index = True)
@@ -77,34 +77,29 @@ def predictionmodel():
          # user inputs in and allow the user to change all of the inputs and submit
          # the entire form at once instead of multiple times
          with st.form('user_inputs'): 
-           island = st.selectbox('Penguin Island', options=[
-             'Biscoe', 'Dream', 'Torgerson']) 
-           sex = st.selectbox('Sex', options=[
-             'Female', 'Male']) 
-           bill_length = st.number_input(
-             'Bill Length (mm)', min_value=0) 
-           bill_depth = st.number_input(
-             'Bill Depth (mm)', min_value=0) 
-           flipper_length = st.number_input(
-             'Flipper Length (mm)', min_value=0) 
-           body_mass = st.number_input(
-             'Body Mass (g)', min_value=0) 
-           st.form_submit_button() 
+                  island = st.selectbox('Penguin Island', options=[
+                           'Biscoe', 'Dream', 'Torgerson']) 
+                  sex = st.selectbox('Sex', options=['Female', 'Male']) 
+                  bill_length = st.number_input('Bill Length (mm)', min_value=0) 
+                  bill_depth = st.number_input('Bill Depth (mm)', min_value=0) 
+                  flipper_length = st.number_input('Flipper Length (mm)', min_value=0) 
+                  body_mass = st.number_input('Body Mass (g)', min_value=0) 
+                  st.form_submit_button() 
 
          # Putting sex and island variables into the correct format
          island_biscoe, island_dream, island_torgerson = 0, 0, 0 
          if island == 'Biscoe': 
-           island_biscoe = 1 
+                  island_biscoe = 1 
          elif island == 'Dream': 
-           island_dream = 1 
+                  island_dream = 1 
          elif island == 'Torgerson': 
-           island_torgerson = 1 
+                  island_torgerson = 1 
 
          sex_female, sex_male = 0, 0 
          if sex == 'Female': 
-           sex_female = 1 
+                  sex_female = 1 
          elif sex == 'Male': 
-           sex_male = 1 
+                  sex_male = 1 
 
          # Create prediction and display it to user
          new_prediction = clf.predict([[bill_length, bill_depth, flipper_length, 

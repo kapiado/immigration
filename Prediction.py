c8672879bc6aa275d92b1e7d374f4b3edda4e884
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-15 23:50:28 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -8,26 +8,23 @@ import pickle
 def user_form():
     st.title('User Form')
 
-    name_placeholder = st.empty()
-    age_placeholder = st.empty()
-    gender_placeholder = st.empty()
+    if 'stage' not in st.session_state:
+        st.session_state.stage = 'input'
 
-    if 'submitted' not in st.session_state:
-        st.session_state.submitted = False
-
-    if not st.session_state.submitted:
-        name = name_placeholder.text_input('Enter your name')
-        age = age_placeholder.slider('Enter your age', 1, 100)
-        gender = gender_placeholder.selectbox('Select your gender', ['Male', 'Female', 'Other'])
+    if st.session_state.stage == 'input':
+        st.session_state.input = {
+            'name': st.text_input('Enter your name'),
+            'age': st.slider('Enter your age', 1, 100),
+            'gender': st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
+        }
 
         if st.button('Submit'):
-            st.session_state.name = name
-            st.session_state.age = age
-            st.session_state.gender = gender
-            st.session_state.submitted = True
+            st.session_state.stage = 'result'
 
-    if st.session_state.submitted:
-        st.success(f"Name: {st.session_state.name}, Age: {st.session_state.age}, Gender: {st.session_state.gender}")
+    elif st.session_state.stage == 'result':
+        input_data = st.session_state.input
+        st.session_state.ret = f"Name: {input_data['name']}, Age: {input_data['age']}, Gender: {input_data['gender']}"
+        st.success(st.session_state.ret)
 
 if __name__ == "__main__":
     user_form()

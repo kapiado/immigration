884d88e3fd8d97f3ec20f564f6e7dae4dd4a6fee
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-15 23:29:48 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -4,6 +4,32 @@
 # Import libraries
 import streamlit as st
 import pickle
+import streamlit.ReportThread as ReportThread
+from streamlit.server.Server import Server
+from session_state import get_state
+
+class SessionState(object):
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+def get_state(hash_funcs=None):
+    """Gets the SessionState for the current session or creates new."""
+    ctx = ReportThread.get_report_ctx()
+
+    this_session = None
+    current_server = Server.get_current()
+    if hasattr(current_server, '_session_infos'):
+        session_infos = Server.get_current()._session_infos.values()
+        for session_info in session_infos:
+            s = session_info.session
+            if s.id == ctx.session_id:
+                this_session = s
+    if this_session is None:
+        raise RuntimeError("Couldn't get your Streamlit Session object.")
+    if not hasattr(this_session, '_custom_session_state'):
+        this_session._custom_session_state = SessionState(**(hash_funcs or {}))
+    return this_session._custom_session_state
 
 # Function to load the model and make predictions
 def load_and_predict(input_data):
@@ -14,26 +40,34 @@ def load_and_predict(input_data):
     # Make predictions using the loaded model (replace this with your model's prediction logic)
     prediction = model.predict(input_data)
     return prediction
+    prediction = 42
+    return prediction
 
 def user_form():
     st.title('User Form')
 
+    # Get session state
+    state = get_state()
+
     # Text Input for Name
-    name = st.text_input('Enter your name')
+    name = st.text_input('Enter your name', key="name")
+    state.name = name
 
     # Slider for Age
-    age = st.slider('Enter your age', 1, 100)
+    age = st.slider('Enter your age', 1, 100, key="age")
+    state.age = age
 
     # Dropdown for Gender
-    gender = st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
+    gender = st.selectbox('Select your gender', ['Male', 'Female', 'Other'], key="gender")
+    state.gender = gender
 
     # Button to submit the form
     if st.button('Submit'):
         # Create a dictionary of user input
         input_data = {
-            'Name': name,
-            'Age': age,
-            'Gender': gender
+            'Name': state.name,
+            'Age': state.age,
+            'Gender': state.gender
         }
 
         # Call the function to load model and make predictions

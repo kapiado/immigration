1a3c2cfec8fcad10a2fcd3e2d6c6c6f09cd9478a
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-15 23:57:12 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -4,24 +4,28 @@
 # Import libraries
 import streamlit as st
 import pickle
+
 def user_form():
-    st.title('User Form')
+    st.title('Streamlit User Form')
 
-    if 'stage' not in st.session_state:
-        st.session_state.stage = 0
+    def user():
+        if 'stage' not in st.session_state:
+            st.session_state.stage = 0
 
-    if st.session_state.stage == 0:
-        name = st.text_input('Enter your name')
-        age = st.slider('Enter your age', 1, 100)
-        gender = st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
+        if st.session_state.stage == 0:
+            name = st.text_input('Enter your name')
+            age = st.slider('Enter your age', 1, 100)
+            gender = st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
 
-        if st.button('Submit'):
-            input_data = {'Name': name, 'Age': age, 'Gender': gender}
-            st.session_state.ret = "Thank you!"
-            st.session_state.stage += 1
+            if st.button('Submit'):
+                input_data = {'Name': name, 'Age': age, 'Gender': gender}
+                st.session_state.ret = "Thank you!"
+                st.session_state.stage += 1
 
-    elif st.session_state.stage > 0:
-        st.write(st.session_state.ret)
+        elif st.session_state.stage > 0:
+            st.write(st.session_state.ret)
+
+    user()
 
 if __name__ == "__main__":
-    user_form()
+    main()

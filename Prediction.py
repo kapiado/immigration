9be88119e664879d7ad9e2f699bb029a449ecf38
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-15 23:51:49 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -12,18 +12,16 @@ def user_form():
         st.session_state.stage = 'input'
 
     if st.session_state.stage == 'input':
-        st.session_state.input = {
-            'name': st.text_input('Enter your name'),
-            'age': st.slider('Enter your age', 1, 100),
-            'gender': st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
-        }
+        name = st.text_input('Enter your name')
+        age = st.slider('Enter your age', 1, 100)
+        gender = st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
 
         if st.button('Submit'):
+            input_data = {'Name': name, 'Age': age, 'Gender': gender}
+            st.session_state.ret = f"Name: {name}, Age: {age}, Gender: {gender}"
             st.session_state.stage = 'result'
 
     elif st.session_state.stage == 'result':
-        input_data = st.session_state.input
-        st.session_state.ret = f"Name: {input_data['name']}, Age: {input_data['age']}, Gender: {input_data['gender']}"
         st.success(st.session_state.ret)
 
 if __name__ == "__main__":

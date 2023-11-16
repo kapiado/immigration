fd1c5557997164e1021f848557eb05cd32ed7f6a
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-15 23:55:43 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -17,11 +17,11 @@ def user_form():
 
         if st.button('Submit'):
             input_data = {'Name': name, 'Age': age, 'Gender': gender}
-            st.session_state.ret = f"Thank you!"
+            st.session_state.ret = "Thank you!"
             st.session_state.stage += 1
 
     elif st.session_state.stage > 0:
-        st.success(st.session_state.ret)
+        st.write(st.session_state.ret)
 
 if __name__ == "__main__":
     user_form()

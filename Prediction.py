e0610eeaa6f5d506a56460b0664614569b9bfb6d
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-15 23:21:03 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -58,6 +58,72 @@ import pickle
 #     elif sex == 'Male': 
 #       sex_male = 1 
 
+def modes(column, df_modes):
+        new_col = column + '_replaced'
+        df_modes[new_col] = False
+        mode = df_modes[column].mode()
+        #print(mode[0])
+        df_modes[column] = df_modes.apply(lambda row: mode[0] if pd.isna(row[column]) else row[column], axis = 1)
+        df_modes[new_col] = df_modes.apply(lambda row: True if pd.isna(row[column]) else False, axis = 1)
+        return df_modes
+def process(query):
+    Info = query
+    df = pd.read_csv('CSV_files/dummieCodex.csv')
+
+    # replace each row with its mode
+    df = modes('AGE', df)
+    df = modes('EDUC', df)
+    df = modes('ETHNIC', df)
+    df = modes('RACE', df)
+    df = modes('GENDER', df)
+    df = modes('MARSTAT', df)
+    df = modes('SAP', df)
+    df = modes('EMPLOY', df)
+    df = modes('LIVARAG', df)
+    df = modes('NUMMHS', df)
+    df = modes('STATEFIP', df)
+
+    headers = ['AGE', 'EDUC', 'ETHNIC', 'RACE', 'GENDER', 'MARSTAT', 'SAP', 'EMPLOY', 'LIVARAG', 'NUMMHS', 'STATEFIP']
+
+    df_query = pd.DataFrame(columns=headers)
+    df_query.loc[0] = query
+
+    def more(column, df_query):
+        new_col = column + '_replaced'
+        df_query[new_col] = False
+
+        df_query[new_col] = df_query.apply(lambda row: True if pd.isna(row[column]) else False, axis = 1)
+        return df_query
+
+    df_query = more('AGE', df_query)
+    df_query = more('EDUC', df_query)
+    df_query = more('ETHNIC', df_query)
+    df_query = more('RACE', df_query)
+    df_query = more('GENDER', df_query)
+    df_query = more('MARSTAT', df_query)
+    df_query = more('SAP', df_query)
+    df_query = more('EMPLOY', df_query)
+    df_query = more('LIVARAG', df_query)
+    df_query = more('NUMMHS', df_query)
+    df_query = more('STATEFIP', df_query)
+
+    first_row = df_query.iloc[0].copy()
+    df.loc[0] = first_row
+    df_codex = df.reset_index(drop=True)
+
+    x = pd.get_dummies(df_codex.drop(columns = ['MH1']), drop_first = True)
+    row = np.array(x.iloc[0]).reshape(1, -1)
+    zeros = np.zeros((1, 10), dtype=int)
+    row = np.concatenate((row, zeros), axis=1)
+
+    loaded_model = joblib.load("pkl_files/dt_clustered_modes.sav")
+
+    y_predicted = loaded_model.predict(row)
+    cluster = y_predicted[0]
+    probs = loaded_model.predict_proba(row)
+
+    displayPrediction(cluster, query, probs)
+
 def interface():
     st.title("Backend Interface")
     if 'stage' not in st.session_state:

74a354622016592ba7d75c8e470ce31f8cd0a25d
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-15 23:43:39 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -6,61 +6,15 @@ import streamlit as st
 import pickle
 
 
-
 def user_form():
-            # Function to load the model and make predictions
-    def load_and_predict(input_data):
-            # Load the pickled model
-            # with open('rf_model.sav', 'rb') as file:
-            #     model = pickle.load(file)
-        
-            # Make predictions using the loaded model (replace this with your model's prediction logic)
-        prediction = st.text('42')
-        return prediction
     st.title('User Form')
 
-    with st.form(key='my_form'):
-        name = st.text_input('Enter your name')
-        age = st.slider('Enter your age', 1, 100)
-        gender = st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
+    name = st.text_input('Enter your name')
+    age = st.slider('Enter your age', 1, 100)
+    gender = st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
 
-        submitted = st.form_submit_button('Submit')
-
-        if submitted:
-            input_data = {
-                'Name': name,
-                'Age': age,
-                'Gender': gender
-            }
-            
-        prediction = load_and_predict(input_data)
-        st.success(f"Prediction: {prediction}")
+    if st.button('Submit'):
+        st.success(f"Name: {name}, Age: {age}, Gender: {gender}")
 
 if __name__ == "__main__":
     user_form()
-
-# def user_form():
-#     st.title('User Form')
-
-#     if 'name' not in st.session_state:
-#         st.session_state.name = st.text_input('Enter your name')
-
-#     if 'age' not in st.session_state:
-#         st.session_state.age = st.slider('Enter your age', 1, 100)
-
-#     if 'gender' not in st.session_state:
-#         st.session_state.gender = st.selectbox('Select your gender', ['Male', 'Female', 'Other'])
-
-#     if st.button('Submit'):
-#         input_data = {
-#             'Name': st.session_state.name,
-#             'Age': st.session_state.age,
-#             'Gender': st.session_state.gender
-#         }
-
-#         prediction = load_and_predict(input_data)
-
-#         st.success(f"Prediction: {prediction}")
-
-# if __name__ == "__main__":
-#     user_form()

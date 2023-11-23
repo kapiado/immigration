da73078e907116a692101780252134128625885c
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-22 21:52:42 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -235,7 +235,7 @@ def interface():
     if st.session_state.stage > 0:
         Info = st.session_state.input
         smt = [Info[0], Info[1], Info[6], Info[7], Info[3], Info[8], Info[9], Info[2], Info[5], Info[10], Info[4]]
-        headers =  ['NAICS Code','Prevailing Wage Level','Prevailing Wage Amount','Work State','Country of Citizenship','Employer Number of Employees','Class of Admission','Job Education','Experience','Months of Experience','Layoff in Past Six Months','Education']
+        headers =  ['NAICS Code','Prevailing Wage Level','Prevailing Wage Amount','Work State','Class of Admission','Country of Citizenship','Employer Number of Employees','Job Education','Experience','Months of Experience','Layoff in Past Six Months','Education']
         data = {}
         for i in range(len(headers)):
             data['Question'] = headers

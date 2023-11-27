82f4b6a54b53febc8b697fb14933e85dd9f40b5a
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-27 13:59:32 -0800

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -255,7 +255,7 @@ def interface():
 
     if st.session_state.stage > 0:
         Info = st.session_state.input
-        smt = [Info[0], Info[1], Info[2], Info[3], Info[4], Info[5], Info[6], Info[7], Info[8], Info[9], Info[10], Info[11]]
+        smt = [Info[0], Info[11], Info[2], Info[3], Info[4], Info[5], Info[6], Info[7], Info[8], Info[9], Info[10], Info[1]]
         headers =  ['NAICS Code','Education Level','Prevailing Wage Level','Prevailing Wage Amount','Class of Admission','Country of Citizenship','Work State','Education Required by Job','Employer Number of Employees','Experience','Months of Experience','Layoff in Past Six Months','Education']
         data = {}
         for i in range(len(headers)):
@@ -268,7 +268,7 @@ def interface():
 
     # def executeQuery(Info)
     def executeQuery():
-        query = [Info[0], Info[1], Info[2], Info[3], Info[4], Info[5], Info[6], Info[7], Info[8], Info[9], Info[10], Info[11]]
+        query = [Info[0], Info[11], Info[2], Info[3], Info[4], Info[5], Info[6], Info[7], Info[8], Info[9], Info[10], Info[1]]
         #st.text('Thank you!')
         process(query)
 
